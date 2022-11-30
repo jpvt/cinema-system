17b4e2fced16919f49b352cda05f97a16e1aff49
João Pedro Vasconcelos Teixeira <jpvt@pop-os.localdomain>
2022-11-30 15:31:58 -0300

    feat #6/ tabela ingressos

---
+++ sql/08_ingressos.sql
@@ -0,0 +1,16 @@
+-- change database and user
+\connect datalake datalakeuser
+
+-- Cria tabela de ingressos
+CREATE TABLE app.ingressos (
+    id_ingresso serial PRIMARY KEY,
+    id_compra serial,
+    id_sessao serial,
+    assento text,
+    tipo_ingresso text,
+
+    CONSTRAINT FK_ingressos_compras FOREIGN KEY(id_compra)
+        REFERENCES compras(id_compra),
+    CONSTRAINT FK_ingressos_sessoes FOREIGN KEY(id_sessao)
+        REFERENCES sessoes(id_sessao)
+);
