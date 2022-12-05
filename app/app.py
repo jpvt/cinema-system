318fd6a95f02ea43e04e71c5b64e2eb6282c9c0b
Itamar Rocha Filho <itamardprf@gmail.com>
2022-12-05 19:24:26 -0300

    Checkout screen and data collection

--- app/app.py
+++ app/app.py
@@ -24,9 +24,9 @@ def getCarrinho(carrinho):
     total = 0.0
     print(carrinho)
     for key in carrinho.keys():
-        carrinho_str += f"{key} - {carrinho[key]['valor_item']:.2f} - {carrinho[key]['quantidade']}\n"
+        carrinho_str += f"{key} - R${carrinho[key]['valor_item']:.2f} - {carrinho[key]['quantidade']}\n"
         total += carrinho[key]['valor_item'] * carrinho[key]['quantidade']
-    carrinho_str += f"Total - {total:.2f}"
+    carrinho_str += f"Total - R${total:.2f}"
 
     return carrinho_str
 
@@ -36,22 +36,30 @@ def addToCarrinho(item, carrinho):
     else:
         carrinho[item["descricao"]]["quantidade"] += 1
 
-if 'carrinho' not in st.session_state:
-    st.session_state.carrinho = {}
+def initialize_data():
+    if 'carrinho' not in st.session_state:
+        st.session_state.customer_data = {}
+        st.session_state.carrinho = {}
+        st.session_state.page = "compras"
+
+initialize_data()
 
 st.title("Cinema Sauro")
 
 st.sidebar.title("Telas")
 select = st.sidebar.selectbox('Escolha a tela que deseja visualizar:', ["Loja","Gerenciamento"])
 
+page = "compras"
+
 if select == "Loja":
+    if st.session_state.page == "compras":
         body = {'query': 'query MyQuery { app_sessoes {  data_sessao  dia_da_semana  tempo_final  tempo_inicio  total_vendido  valor_inteira  filme {   nome   atores_principais   categoria   censura   descricao  }  ingressos {   assento  } }}'}
         session = requests.post(ENDPOINT, json=body, headers=headers)
 
         body = {'query': 'query MyQuery { app_itens {  descricao  valor_item }}'}
         items = requests.post(ENDPOINT, json=body, headers=headers)
 
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
 
         col1.subheader('Ingressos')
         col1.code(json.dumps(session.json()['data']['app_sessoes'], indent=2))
@@ -64,8 +72,42 @@ if select == "Loja":
                 if selected_item == item["descricao"]:
                     addToCarrinho(item, st.session_state.carrinho)
 
-        col3.subheader('Carrinho')
-        col3.code(getCarrinho(st.session_state.carrinho))
+        col2.subheader('Carrinho')
+        col2.code(getCarrinho(st.session_state.carrinho))
+
+        if col2.button('Fazer checkout'):
+            st.session_state.page = "checkout"
+
+    elif st.session_state.page == "checkout":
+
+        st.subheader('Preencher informações de compra')
+
+        if st.button('Voltar para compras'):
+            st.session_state.page = "compras"
+        
+        with st.form("my_form"):
+            col1, col2 = st.columns(2)
+            
+            nome = col1.text_input(label='nome')
+            CPF = col1.text_input(label='CPF')
+            telefone = col1.text_input(label='telefone')
+            forma_de_pagamento = col1.selectbox(label="Forma de pagamento", options=["crédito", "débito", "dinheiro", "pix"])
+
+            # Every form must have a submit button.
+
+            col2.subheader('Carrinho')
+            col2.code(getCarrinho(st.session_state.carrinho))
+
+            submitted = col2.form_submit_button("Finalizar compra")
+            if submitted:
+                print(nome, CPF, telefone)
+                st.session_state.customer_data = {"nome": nome, "CPF": CPF, "telefone": telefone}
+                st.session_state.page = f"thank_you"
+
+    elif st.session_state.page == "thank_you":
+        st.subheader(f"Obrigado por comprar conosco, {st.session_state.customer_data['nome']}!")
+        if st.button('Fazer nova compra'):
+            st.session_state.page = "compras"
 
 elif select == "Gerenciamento":
     print('abc')
